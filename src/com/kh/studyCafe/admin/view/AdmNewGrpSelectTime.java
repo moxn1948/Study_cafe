90e62e4795fdd36addb73e28c335b1c8bdbf0374
moxn <moxn1948@gmail.com>
2019-09-03 13:16:31 +0900

    GUI 팝업 수정 및 User 클래스 수정 _ 조문정

--- src/com/kh/studyCafe/admin/view/AdmNewGrpSelectTime.java
+++ src/com/kh/studyCafe/admin/view/AdmNewGrpSelectTime.java
@@ -6,169 +6,179 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
 public class AdmNewGrpSelectTime extends JPanel implements ActionListener{
 	
 	public AdmNewGrpSelectTime() {
-
-		// 패널 설정
-		int w = 540;
-		int h = 467;
-		int x = popPosition(w, h)[0];
-		int y = popPosition(w, h)[1];
-		
-		this.setBounds(x, y, w, h); 
-		this.setBackground(new Color(239, 234, 222));
-		this.setBorder(BorderFactory.createLineBorder(new Color(189, 177, 157)));
-
-		// title 텍스트 설정
-		JLabel title = new JLabel("1일권");
-
-		title.setLocation(230, 34);
-		title.setForeground(new Color(127, 118, 104));
-		title.setFont(new Font("맑은 고딕", Font.BOLD, 32));
-		title.setSize(title.getPreferredSize());
-
-		// 인원 텍스트 설정
-		JLabel userCount = new JLabel("인원");
-
-		userCount.setLocation(114, 104);
-		userCount.setForeground(new Color(127, 118, 104));
-		userCount.setFont(new Font("맑은 고딕", Font.BOLD, 28));
-		userCount.setSize(userCount.getPreferredSize());
-		
-		// 총 인원 표시
-		JLabel totalUserCount = new JLabel("총 인원");
-
-		totalUserCount.setLocation(90, 170);
-		totalUserCount.setForeground(new Color(127, 118, 104));
-		totalUserCount.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		totalUserCount.setSize(totalUserCount.getPreferredSize());
-		
-		// 총 인원 - 명수 표시
-		JLabel totalUserCountNum = new JLabel("2명");
-
-		totalUserCountNum.setLocation(164, 170);
-		totalUserCountNum.setForeground(new Color(127, 118, 104));
-		totalUserCountNum.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		totalUserCountNum.setSize(totalUserCountNum.getPreferredSize());
-
-		// 총 시간 표시
-		JLabel totalUserTime = new JLabel("이용하실 시간");
-
-		totalUserTime.setLocation(300, 170);
-		totalUserTime.setForeground(new Color(127, 118, 104));
-		totalUserTime.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		totalUserTime.setSize(totalUserTime.getPreferredSize());
-		
-		// 총 시간 - 시간 표시
-		JLabel totalUserTimeNum = new JLabel("02:00");
-
-		totalUserTimeNum.setLocation(434, 170);
-		totalUserTimeNum.setForeground(new Color(127, 118, 104));
-		totalUserTimeNum.setFont(new Font("맑은 고딕", Font.BOLD, 20));
-		totalUserTimeNum.setSize(totalUserTimeNum.getPreferredSize());
-		
-		
-		// 시간 텍스트 설정
-		JLabel userTime = new JLabel("시간");
-
-		userTime.setLocation(359, 104);
-		userTime.setForeground(new Color(127, 118, 104));
-		userTime.setFont(new Font("맑은 고딕", Font.BOLD, 28));
-		userTime.setSize(userTime.getPreferredSize());
+	      // 패널 설정
+	      int w = 731;
+	      int h = 474;
+	      int x = popPosition(w, h)[0];
+	      int y = popPosition(w, h)[1];
+	      
+	      this.setBounds(x, y, w, h); 
+	      this.setBackground(new Color(239, 234, 222));
+	      this.setBorder(BorderFactory.createLineBorder(new Color(163, 152, 134)));
 
 
-		// +, - 버튼 설정
-		JButton countUpBtn = new JButton("+");
-		JButton countDownBtn = new JButton("-");
-		
-		countUpBtn.setBounds(44, 234, 200, 44);
-		countDownBtn.setBounds(44, 330, 200, 44);
-		countUpBtn.setBackground(new Color(127, 118, 104));
-		countDownBtn.setBackground(new Color(127, 118, 104));
-		countUpBtn.setForeground(Color.WHITE);
-		countUpBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		countDownBtn.setForeground(Color.WHITE);
-		countDownBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		countUpBtn.setBorder(BorderFactory.createLineBorder(new Color(189, 177, 157)));
-		countDownBtn.setBorder(BorderFactory.createLineBorder(new Color(163, 152, 134)));
+	      JLayeredPane hourLayer = new JLayeredPane();
+	      hourLayer.setBounds(30,28,334,349);
+	      hourLayer.setBorder(BorderFactory.createLineBorder(new Color(127, 118, 104)));
+	      
+	      // 인원 텍스트 설정
+	      JRadioButton userCount = new JRadioButton("인원");
+	      
+	      userCount.setBackground(new Color(239, 234, 222));
+	      userCount.setLocation(124, 34);
+	      userCount.setForeground(new Color(127, 118, 104));
+	      userCount.setFont(new Font("맑은 고딕", Font.BOLD, 28));
+	      userCount.setSize(userCount.getPreferredSize());
+	      
+	      // 총 인원 표시
+	      JLabel totalUserCount = new JLabel("총 인원");
 
-		JButton timeUpBtn = new JButton("+");
-		JButton timeDownBtn = new JButton("-");
+	      totalUserCount.setLocation(113, 102);
+	      totalUserCount.setForeground(new Color(127, 118, 104));
+	      totalUserCount.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+	      totalUserCount.setSize(totalUserCount.getPreferredSize());
+	      
+	      // 총 인원 - 명수 표시
+	      JLabel totalUserCountNum = new JLabel("2명");
 
-		timeUpBtn.setBounds(294, 234, 200, 44);
-		timeDownBtn.setBounds(294, 330, 200, 44);
-		timeUpBtn.setBackground(new Color(127, 118, 104));
-		timeDownBtn.setBackground(new Color(127, 118, 104));
-		timeUpBtn.setForeground(Color.WHITE);
-		timeUpBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		timeDownBtn.setForeground(Color.WHITE);
-		timeDownBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
-		timeUpBtn.setBorder(BorderFactory.createLineBorder(new Color(189, 177, 157)));
-		timeDownBtn.setBorder(BorderFactory.createLineBorder(new Color(163, 152, 134)));
+	      totalUserCountNum.setLocation(187, 102);
+	      totalUserCountNum.setForeground(new Color(127, 118, 104));
+	      totalUserCountNum.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+	      totalUserCountNum.setSize(totalUserCountNum.getPreferredSize());
 
-		// 인원 표시
-		JTextField countDisplay = new JTextField("2");
-		JTextField timeDisplay = new JTextField("02:00");
-		
+	      // +, - 버튼 설정
+	      JButton countUpBtn = new JButton("+");
+	      JButton countDownBtn = new JButton("-");
+	      
+	      countUpBtn.setBounds(66, 166, 200, 44);
+	      countDownBtn.setBounds(66, 262, 200, 44);
+	      countUpBtn.setBackground(new Color(127, 118, 104));
+	      countDownBtn.setBackground(new Color(127, 118, 104));
+	      countUpBtn.setForeground(Color.WHITE);
+	      countUpBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
+	      countDownBtn.setForeground(Color.WHITE);
+	      countDownBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
+	      countUpBtn.setBorder(BorderFactory.createLineBorder(new Color(189, 177, 157)));
+	      countDownBtn.setBorder(BorderFactory.createLineBorder(new Color(163, 152, 134)));
+	      
+	      
+	      // 인원 표시
+	      JTextField countDisplay = new JTextField("2");
+	      countDisplay.setBounds(66, 216, 200, 40);
+	      countDisplay.setBackground(Color.WHITE);
+	      countDisplay.setForeground(new Color(127, 118, 104));
+	      countDisplay.setFont(new Font("맑은 고딕", Font.BOLD, 22));
+	      countDisplay.setBorder(BorderFactory.createEmptyBorder());
+	      countDisplay.setHorizontalAlignment(JTextField.CENTER);
+	      
+	      
+	      hourLayer.add(userCount);
+	      hourLayer.add(totalUserCount); 
+	      hourLayer.add(totalUserCountNum);
+	      hourLayer.add(countUpBtn);
+	      hourLayer.add(countDownBtn);
+	      hourLayer.add(countDisplay);
+	      
 
-		countDisplay.setBounds(44, 284, 200, 40);
-		countDisplay.setBackground(Color.WHITE);
-		countDisplay.setForeground(new Color(127, 118, 104));
-		countDisplay.setFont(new Font("맑은 고딕", Font.BOLD, 22));
-		countDisplay.setBorder(BorderFactory.createEmptyBorder());
-		countDisplay.setHorizontalAlignment(JTextField.CENTER);
+	      JLayeredPane weekLayer = new JLayeredPane();
+	      weekLayer.setBounds(368,28,334,349);
+	      weekLayer.setBorder(BorderFactory.createLineBorder(new Color(127, 118, 104)));
+
+	      // 시간 텍스트 설정
+	      JRadioButton userTime = new JRadioButton("시간");
+
+	      userTime.setBackground(new Color(239, 234, 222));
+
+	      userTime.setLocation(125, 36);
+	      userTime.setForeground(new Color(127, 118, 104));
+	      userTime.setFont(new Font("맑은 고딕", Font.BOLD, 28));
+	      userTime.setSize(userTime.getPreferredSize());
 
 
-		timeDisplay.setBounds(294, 284, 200, 40);
-		timeDisplay.setBackground(Color.WHITE);
-		timeDisplay.setForeground(new Color(127, 118, 104));
-		timeDisplay.setFont(new Font("맑은 고딕", Font.BOLD, 22));
-		timeDisplay.setBorder(BorderFactory.createEmptyBorder());
-		timeDisplay.setHorizontalAlignment(JTextField.CENTER);
-		
-		
-		
-		// 버튼 설정
-		JButton cancelBtn = new JButton("Cancel");
-		JButton confirmBtn = new JButton("Confirm");
-		
-		cancelBtn.setBounds(25, 394, 240, 50);
-		confirmBtn.setBounds(275,394, 240, 50);
-		cancelBtn.setBackground(new Color(189, 177, 157));
-		confirmBtn.setBackground(new Color(163, 152, 134));
-		cancelBtn.setForeground(Color.WHITE);
-		cancelBtn.setFont(new Font("맑은 고딕", Font.BOLD, 18));
-		confirmBtn.setForeground(Color.WHITE);
-		confirmBtn.setFont(new Font("맑은 고딕", Font.BOLD, 18));
-		cancelBtn.setBorder(BorderFactory.createLineBorder(new Color(189, 177, 157)));
-		confirmBtn.setBorder(BorderFactory.createLineBorder(new Color(163, 152, 134)));
-		
-		
+	      // 총 시간 표시
+	      JLabel totalUserTime = new JLabel("이용하실 시간");
 
-		this.add(title);
-		this.add(userCount);
-		this.add(userTime);
-		this.add(totalUserCount);
-		this.add(totalUserCountNum);
-		this.add(totalUserTime);
-		this.add(totalUserTimeNum);
-		
-		this.add(countUpBtn);
-		this.add(countDownBtn);
-		this.add(countDisplay);
-		
-		this.add(timeUpBtn);
-		this.add(timeDownBtn);
-		this.add(timeDisplay);
-		
-		this.add(cancelBtn);
-		this.add(confirmBtn);
+	      totalUserTime.setLocation(66, 102);
+	      totalUserTime.setForeground(new Color(127, 118, 104));
+	      totalUserTime.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+	      totalUserTime.setSize(totalUserTime.getPreferredSize());
+	      
+	      // 총 시간 - 시간 표시
+	      JLabel totalUserTimeNum = new JLabel("02:00");
+
+	      totalUserTimeNum.setLocation(207, 102);
+	      totalUserTimeNum.setForeground(new Color(127, 118, 104));
+	      totalUserTimeNum.setFont(new Font("맑은 고딕", Font.BOLD, 20));
+	      totalUserTimeNum.setSize(totalUserTimeNum.getPreferredSize());
+	      
+	      // +, - 버튼 설정
+	      JButton timeUpBtn = new JButton("+");
+	      JButton timeDownBtn = new JButton("-");
+
+	      timeUpBtn.setBounds(66, 166, 200, 44);
+	      timeDownBtn.setBounds(66, 262, 200, 44);
+	      timeUpBtn.setBackground(new Color(127, 118, 104));
+	      timeDownBtn.setBackground(new Color(127, 118, 104));
+	      timeUpBtn.setForeground(Color.WHITE);
+	      timeUpBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
+	      timeDownBtn.setForeground(Color.WHITE);
+	      timeDownBtn.setFont(new Font("맑은 고딕", Font.BOLD, 30));
+	      timeUpBtn.setBorder(BorderFactory.createLineBorder(new Color(189, 177, 157)));
+	      timeDownBtn.setBorder(BorderFactory.createLineBorder(new Color(163, 152, 134)));
+
+	      
+	      // 시간 표시
+	      JTextField timeDisplay = new JTextField("02:00");
+
+	      timeDisplay.setBounds(66, 216, 200, 40);
+	      timeDisplay.setBackground(Color.WHITE);
+	      timeDisplay.setForeground(new Color(127, 118, 104));
+	      timeDisplay.setFont(new Font("맑은 고딕", Font.BOLD, 22));
+	      timeDisplay.setBorder(BorderFactory.createEmptyBorder());
+	      timeDisplay.setHorizontalAlignment(JTextField.CENTER);
+
+	      weekLayer.add(userTime);
+	      weekLayer.add(totalUserTime);
+	      weekLayer.add(totalUserTimeNum);
+	      weekLayer.add(timeUpBtn);
+	      weekLayer.add(timeDownBtn);
+	      weekLayer.add(timeDisplay);   
+	      
+	      // 버튼 설정
+	      JButton cancelBtn = new JButton("Cancel");
+	      JButton confirmBtn = new JButton("Confirm");
+
+	      cancelBtn.setBounds(30, 390, 334, 50);
+	      confirmBtn.setBounds(368,390, 334, 50);
+	      cancelBtn.setBackground(new Color(189, 177, 157));
+	      confirmBtn.setBackground(new Color(163, 152, 134));
+	      cancelBtn.setForeground(Color.WHITE);
+	      cancelBtn.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+	      confirmBtn.setForeground(Color.WHITE);
+	      confirmBtn.setFont(new Font("맑은 고딕", Font.BOLD, 18));
+	      cancelBtn.setBorder(BorderFactory.createLineBorder(new Color(189, 177, 157)));
+	      confirmBtn.setBorder(BorderFactory.createLineBorder(new Color(163, 152, 134)));
+	      
+	      ButtonGroup timeRadio = new ButtonGroup();
+	      timeRadio.add(userCount);
+	      timeRadio.add(userTime);
+	      
+	      this.add(hourLayer);
+	      this.add(weekLayer);
+	      
+	      this.add(cancelBtn);
+	      this.add(confirmBtn);
 		
 	}
 
--- src/com/kh/studyCafe/model/vo/User.java
+++ src/com/kh/studyCafe/model/vo/User.java
@@ -15,6 +15,7 @@ public class User {
 	private final int GRPSEAT = 2; // 그룹좌석 사용할 때
 	private int seatType = NOSEAT;
 	private int point;
+	private Date pointTime; // 누적 결제시간
 	private String rank;
 	
 	public User() {}
@@ -29,7 +30,7 @@ public class User {
 
 	// 모든 필드 초기화 생성자
 	public User(String name, String phoneNum, String password, int seatNum, Date inTime, Date outTime, Date remainTime,
-			int seatType, int point, String rank) {
+			int seatType, int point, Date pointTime, String rank) {
 		super();
 		this.name = name;
 		this.phoneNum = phoneNum;
@@ -40,6 +41,7 @@ public class User {
 		this.remainTime = remainTime;
 		this.seatType = seatType;
 		this.point = point;
+		this.pointTime = pointTime;
 		this.rank = rank;
 	}
 	
@@ -114,6 +116,14 @@ public class User {
 
 	public void setPoint(int point) {
 		this.point = point;
+	}
+	
+	public Date getPointTime() {
+		return pointTime;
+	}
+
+	public void setPointTime(Date pointTime) {
+		this.pointTime = pointTime;
 	}
 
 	public String getRank() {
