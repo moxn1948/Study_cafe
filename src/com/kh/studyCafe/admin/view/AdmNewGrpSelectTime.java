f9cabad3e01706a5536ebe0771211b2e0e558033
sbs <sbs@DESKTOP-3R6P3N0>
2019-09-15 06:25:00 +0900

    관리자, 키오스크 연동 디버깅후 오류 수정 - 서범수

--- src/com/kh/studyCafe/admin/view/AdmNewGrpSelectTime.java
+++ src/com/kh/studyCafe/admin/view/AdmNewGrpSelectTime.java
@@ -317,6 +317,7 @@ public class AdmNewGrpSelectTime extends JPanel implements ActionListener{
 //			cp.changeTablePanel3(mf, this, op, op2, new AdmAllUserList(mf, new AdmManager().usingUserManager(), new AdmDao().admRead(), client));
 
 			mf.remove(this);
+			mf.remove(op);
 			mf.remove(op2);
 			
 			// 메인프레임, 현재 팝업 패널, 테이블 패널, 뒤에 깔린 팝업패널, 새로 생성할 테이블 패널
